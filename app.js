6c861e2e5ea6ffc3b8c8ad743e84fcfd36fda2c2
Hakob Andreasyan <77132808+handreasyan@users.noreply.github.com>
2021-06-16 00:24:15 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const input = document.querySelector(".input");
+const result = document.querySelector(".result");
+
+let id;
+input.addEventListener("input", (ev) => {
+  let val = ev.target.value;
+  if (id) clearTimeout(id);
+  id = setTimeout(() => {
+    input.setAttribute("disabled", "true");
+    fetch(`https://goweather.herokuapp.com/weather/${val}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((res) => {
+        input.removeAttribute("disabled");
+        if (res.temperature !== "") {
+          result.innerHTML = `In ${val} is ${res.temperature}`;
+        } else {
+          result.innerHTML = `Please enter the valid City Name`;
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, 1000);
+});
